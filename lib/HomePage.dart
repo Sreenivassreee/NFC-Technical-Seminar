b2e32d81051a3b3a6762c4726621522c0bc82107
SreenivasSreee <ksreenivascse@gmail.com>
2021-03-24 19:56:15 +0530

    initial commit

---
+++ lib/HomePage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  BookRouterDelegate _routerDelegate = BookRouterDelegate();
+  BookRouteInformationParser _routeInformationParser =
+      BookRouteInformationParser();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Books App',
+      routerDelegate: _routerDelegate,
+      routeInformationParser: _routeInformationParser,
+    );
+  }
+}
